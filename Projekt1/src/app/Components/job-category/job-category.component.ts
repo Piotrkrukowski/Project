08d6e54533153b5af026cade86b2d592587e1e96
Piotr Kukowski <piotr.kukowski@assecods.pl>
2024-06-04 13:26:20 +0200

    Develop V2

---
+++ Projekt1/src/app/Components/job-category/job-category.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { JobServicesService } from '../../Services/Features/job-services.service';
+
+@Component({
+  selector: 'app-job-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-category.component.html',
+  styleUrl: './job-category.component.css'
+})
+export class JobCategoryComponent implements OnInit {
+
+  constructor(private jobService: JobServicesService){
+
+  }
+
+  public data:any;
+
+  ngOnInit(): void {
+    this.jobService.fetchCategories().subscribe((result:any)=>{
+      console.log(result);
+      this.data=result.results;
+    }
+  )}
+}
