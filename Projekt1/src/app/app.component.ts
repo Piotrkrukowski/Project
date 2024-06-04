378c0c173ec96ee3f03e1b4f4469f93891aa7623
Piotr Kukowski <piotr.kukowski@assecods.pl>
2024-06-04 12:24:10 +0200

    Develop

--- Projekt1/src/app/app.component.ts
+++ Projekt1/src/app/app.component.ts
@@ -1,24 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { JobServicesService } from './Services/Features/job-services.service';
+import { JobCategoryComponent } from './Components/job-category/job-category.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,JobCategoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent{
   title = 'Projek12';
-
-  constructor(private jobService: JobServicesService){
-
-  }
-
-  ngOnInit(): void {
-    this.jobService.fetchPost().subscribe((result:any)=>{
-      console.log(result);
-    })
-  }
 }
